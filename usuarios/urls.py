787f4fa94009ec8051298482380f869f420301f6
Ricardo Silva <7474175+romstk@users.noreply.github.com>
2025-01-07 23:58:13 -0300

    Aula 3 do curso de autenticação de formulários. Implementamos funcionalidades de login, cadastro de usuários e logout, utilizando os usuários na tabela auth_user do sqlite3, implementadno as funcionalidades para inclusão de usuários no banco de dados, login e logout dos mesmos no banco de dados com as funcionalidades próprias para estas finalidades direto nos formulários que criamos para estes fins.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from usuarios.views import login, cadastro
+from usuarios.views import login, cadastro, logout
 
 
 #Criamos uma lista que será responsável por gerenciar as rotas/urls do app usuários para exibir a view correspondente 
 urlpatterns = [
         path('login', login, name= 'login'),
-        path('cadastro', cadastro, name= 'cadastro')
+        path('cadastro', cadastro, name= 'cadastro'),
+        path('logout', logout, name='logout'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,77 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
+
+
 
 def login(request):
     #instacia da classe LoginFrom
     form = LoginForms()
+
+    if request.method == 'POST': 
+       #puxando os dados do formulário postado para uma variável.
+       form = LoginForms(request.POST)
+
+       #validando o formulário
+       if form.is_valid():
+         nome = form["nome_login"].value()
+         senha = form["senha"].value()
+       #neste ponto faremos a validação do usuário passando para um método do Django que fará a autenticação
+       usuario = auth.authenticate(
+           request, 
+           username=nome,
+           password=senha
+        ) 
+       #se o usuário for válido passo os parâmetros request e o usuário que vamos autenticar
+       if usuario is not None:
+           auth.login(request, usuario)
+           messages.success(request, "Usuário logado com sucesso! ")
+           return redirect('home')
+       else:
+           messages.error(request, "erro ao efetuar login")
+           return redirect('login')
+            
     #Ao renderizar o template passamos um dicionário com o objeto de form instanciado
     return render(request, "usuarios/login.html", {"form": form})
 
 def cadastro(request):
     form = CadastroForms()
+
+    #testamos qual o método que chamou esta view, se for post vamos executar a ação de armazenar os dados cadastrados. 
+    if request.method == 'POST': 
+        #armazeno os dados do formulário enviados por post na variável form
+        form = CadastroForms(request.POST)
+
+        if form.is_valid():
+            if form['senha_1'].value() != form['senha_2'].value():
+                #se senha e confirmações não forem iguais o sistema redireciona para a página de cadastro novamente
+                messages.error(request, "Sennha e confirmação de senha devem ser iguais.")
+                return redirect('cadastro') 
+            #atribuindo as variáveis com dados dos campos do forms.
+            nome = form["nome_cadastro"].value()
+            email = form["email"].value()
+            senha = form["senha_1"].value()
+
+            #vamos testar agora se o usuário a ser cadastrado já existe, se existir redireciona para a página de cadastro novamente para cadastrar novo usuário
+            if User.objects.filter(username=nome).exists():
+                messages.error(request, "Usuário já existente. ")
+                return redirect('cadastro')
+            
+            #após todas as validações crio um novo usuário de fato
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
+            messages.success(request, "Usuário cadastrado com sucesso. Já pode fazer o login. ")
+            return redirect('login')
+
+    #este return vai ser chamado somente se o método do request for diferente de POST, ou seja, se o formulário for chamado para cadastrar novo usuário 
     return render(request, "usuarios/cadastro.html", {"form": form})
 
+def logout(request):
+    auth.logout
+    messages.info(request, "Logout efetuado! ")
+    return redirect('login')
